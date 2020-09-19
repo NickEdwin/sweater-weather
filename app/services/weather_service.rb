853b20e7b825a5cee514b404741a6dadf5898d23
Nick Edwin <nickedwin85@gmail.com>
2020-09-19 14:24:22 -0600

    Updated API call to exclude minute by minute weather reponse

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -1,6 +1,6 @@
 class WeatherService
   def forecast(lat_long)
-    response = conn.get("data/2.5/onecall?lat=#{lat_long[:lat]}&lon=#{lat_long[:lng]}&appid=#{ENV['ENV_WEATHERMAP_KEY']}")
+    response = conn.get("data/2.5/onecall?lat=#{lat_long[:lat]}&lon=#{lat_long[:lng]}&exclude=minutely&appid=#{ENV['ENV_WEATHERMAP_KEY']}")
     JSON.parse(response.body, symbolize_names: true)
   end
 
