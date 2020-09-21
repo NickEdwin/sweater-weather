f3f93bf08366843a78f36784a0c259cff55f522f
Nick Edwin <nickedwin85@gmail.com>
2020-09-21 14:51:48 -0600

    Added api_key to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates_presence_of :password, require: true
+  validates_presence_of :api_key, require: true
 
   has_secure_password
 end
