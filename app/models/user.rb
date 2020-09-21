a5e5537c41793f7d5b265c5e2f1c9b935a1e05e9
Nick Edwin <nickedwin85@gmail.com>
2020-09-21 14:41:01 -0600

    User model information

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  validates :email, uniqueness: true, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates_presence_of :password, require: true
+
+  has_secure_password
+end
