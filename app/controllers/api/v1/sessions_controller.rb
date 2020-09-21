a054fc55cd767ce0126d0dfaf596e5d3676f802b
Nick Edwin <nickedwin85@gmail.com>
2020-09-21 17:40:27 -0600

    Added session creation

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::SessionsController < ApplicationController
   def create
     if user = User.find_by_email(user_params[:email])
       if user.authenticate(user_params[:password])
+        session[:user_id] = user.id
         render json: {"data":
           {
             "type": "users",
