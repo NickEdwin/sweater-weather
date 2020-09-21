fe38abe39aec421d5f8cafccb8695199b1afdb7a
Nick Edwin <nickedwin85@gmail.com>
2020-09-21 16:33:18 -0600

    User create method

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,36 @@
+require 'securerandom'
+
+class Api::V1::UsersController < ApplicationController
+  def create
+    @new_user = User.new(
+      email: params[:email],
+      password: params[:password],
+      api_key: SecureRandom.hex(13)
+      )
+    if @new_user.valid?
+      @new_user.save
+      render json: {"data":
+        {
+          "type": "users",
+          "id": @new_user.id,
+          "attributes": {
+            "email": @new_user.email,
+            "api_key": @new_user.api_key
+            }
+          }
+        }, status: 201
+    else
+      render json: {"data":
+        {
+          "errors": @new_user.errors.full_messages
+        }
+      }, status: 400
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password)
+  end
+end
