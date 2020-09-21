fd889260debdc3e7b02702349449349741cc4a53
Nick Edwin <nickedwin85@gmail.com>
2020-09-21 09:12:10 -0600

    Initial routes for climbing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/forecast', to: 'forecasts#show'
+      get '/climbing_routes', to: 'routes#show'
     end
   end
 end
