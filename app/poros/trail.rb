132063f9ac0d3a0b768b1c8c520ea6437326b742
Nick Edwin <nickedwin85@gmail.com>
2020-09-21 11:16:30 -0600

    Updated trail poro

--- app/poros/trail.rb
+++ app/poros/trail.rb
@@ -1,6 +1,24 @@
 class Trail
   def self.search(lat_long)
     trail_service = TrailService.new
-    trail_service.search(lat_long)
+    trail_info = trail_service.search(lat_long)
+
+    @trails  = trail_info[:routes].map do |trail|
+      Trail.new(trail)
+    end
+  end
+
+
+  attr_reader :name,
+              :type,
+              :rating,
+              :location
+
+
+  def initialize(trail_info)
+    @name = trail_info[:name]
+    @type = trail_info[:type]
+    @rating = trail_info[:rating]
+    @location = trail_info[:location]
   end
 end
