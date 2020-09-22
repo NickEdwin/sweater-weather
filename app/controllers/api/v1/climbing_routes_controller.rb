c13b34abafe2517d9d4f6c8f43edf03239426a22
Nick Edwin <nickedwin85@gmail.com>
2020-09-22 09:34:00 -0600

    Initial rework for using Serializer for trail json response

--- app/controllers/api/v1/climbing_routes_controller.rb
+++ app/controllers/api/v1/climbing_routes_controller.rb
@@ -1,14 +1,5 @@
 class Api::V1::ClimbingRoutesController < ApplicationController
   def show
-    lat_long = Location.conversion(params[:location])
-    weather = Weather.forecast(lat_long)
-    @trails = Trail.search(lat_long)
-
-    render json: {
-      id: "null", type: "climbing_route",
-      attributes: {location: params[:location],
-        forecast: {summary: weather[:current][:weather][0][:description], temperature: weather[:current][:feels_like]}},
-      routes: @trails
-    }
+    render json: TrailSerializer.all_trails(params[:location])
   end
 end
