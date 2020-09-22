37166c08cfac091681ca931235bcff408fc35857
Nick Edwin <nickedwin85@gmail.com>
2020-09-22 11:18:12 -0600

    Added additional test to match vcr for weather conditions

--- spec/requests/api/v1/landing_page/weather_spec.rb
+++ spec/requests/api/v1/landing_page/weather_spec.rb
@@ -23,4 +23,21 @@ describe "Weather API" do
 
     expect(data["minutely"]).to be(nil)
   end
+
+  it "Returns correct weather details", :vcr do
+    get '/api/v1/forecast?location=denver,co'
+    data = JSON.parse(response.body)
+    
+    expect(data["current"]["sunrise"]).to be(1600692427)
+    expect(data["current"]["sunset"]).to be(1600736311)
+    expect(data["current"]["temp"]).to be(84.07)
+    expect(data["current"]["feels_like"]).to be(74.23)
+    expect(data["current"]["pressure"]).to be(1023)
+    expect(data["current"]["humidity"]).to be(16)
+    expect(data["current"]["dew_point"]).to be(33.1)
+    expect(data["current"]["uvi"]).to be(6.87)
+    expect(data["current"]["clouds"]).to be(40)
+    expect(data["current"]["visibility"]).to be(10000)
+    expect(data["current"]["wind_speed"]).to be(11.41)
+  end
 end
