31d512ced99fb33dede091409bb00d0df26f37ef
Nick Edwin <nickedwin85@gmail.com>
2020-09-22 14:32:09 -0600

    Updated for strong params

--- app/controllers/api/v1/road_trips_controller.rb
+++ app/controllers/api/v1/road_trips_controller.rb
@@ -1,25 +1,25 @@
 class Api::V1::RoadTripsController < ApplicationController
   def show
-    travel_time = Location.road_trip(params[:origin], params[:destination])
-    lat_long = Location.conversion(params[:destination])
+    travel_time = Location.road_trip(user_params[:origin], user_params[:destination])
+    lat_long = Location.conversion(user_params[:destination])
     weather = Weather.forecast(lat_long)
 
-    user = User.where(api_key: params[:api_key])
-    
+    user = User.where(api_key: user_params[:api_key])
+
     if user.any?
       render json: {data:
       {
         type: "trip",
         id: "null",
         attributes: {
-          origin: params[:origin],
-          destination: params[:destination],
+          origin: user_params[:origin],
+          destination: user_params[:destination],
           travel_time_mins: travel_time,
           forecast: {
             temperature: weather[:current][:temp],
             description: weather[:current][:weather][0][:description]
           },
-          api_key: params[:api_key]
+          api_key: user_params[:api_key]
           }
         }
       }
