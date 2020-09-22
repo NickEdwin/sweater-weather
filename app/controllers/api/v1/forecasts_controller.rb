ea7a068bb2a877efc7508cd931a7cd3db0778862
Nick Edwin <nickedwin85@gmail.com>
2020-09-22 09:07:32 -0600

    Added background image controller and poro to fix broken tests

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::ForecastsController < ApplicationController
   def show
     @lat_long = Location.conversion(params[:location])
-    @image = Image.seach(params[:location])
+
     @weather = Weather.forecast(@lat_long)
     render json: @weather
   end
